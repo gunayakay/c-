f71e4b8988fa6f70a153a915a4391aa724c73560
gunayakay <gunayakay@outlook.com>
2023-01-24 23:34:48 +0300

    nested loops

--- MyFirstName/Program.cs
+++ MyFirstName/Program.cs
@@ -7,21 +7,32 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
-            // for loop = repeats some code a FINITE amounts of times
+            // nested loops =  loops inside of other loops 
+            //                 Uses vary. Used a lot in sorting alghorithms
 
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine(i);
-            }
 
-            // countdown 
-            for (int i = 10; i > 0; i--)
-            {
-                Console.WriteLine(i);
-            }
-                Console.WriteLine("Happy new year!!");
+
+            Console.Write("How many rows?: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("How many columns?: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("What symbol?: ");
+            string symbol= Console.ReadLine();
 
             
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        Console.Write(symbol);
+                    }
+                Console.WriteLine();
+            }
+            
+            
+
             Console.ReadKey();
         }
 
