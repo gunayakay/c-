fcafdc2c4128d8dcb7428dd03ad65abbc79e1c9e
gunayakay <gunayakay@outlook.com>
2023-01-28 01:32:03 +0300

    foreach loop

--- MyFirstName/Program.cs
+++ MyFirstName/Program.cs
@@ -7,27 +7,20 @@ namespace RockPaperScissors
     {
         static void Main(string[] args)
         {
-            //array = a variable that can store multiple values. fixed sizes
+            // foreach loop = a simpler way to itarate over an array, but it's less flexible
 
-            //String[] cars = {"BMW" ,"Mustang","Corvette" };
-            String[] cars = new string[3];
-            //Console.WriteLine(cars);
-            //Console.WriteLine(cars[]);
-            //Console.WriteLine(cars[0]);
-            //Console.WriteLine(cars[1]);
-            //Console.WriteLine(cars[2]);
+            String[] cars = { "BMW", "Tesla", "Corvette" };
 
-            cars[0] = "Tesla";
-            cars[1] = "Mustang";
-            cars[2] = "Corevette";
-
-            for (int i = 0; i < cars.Length ; i++)
+            for (int i = 0; i < cars.Length; i++)
             {
                 Console.WriteLine(cars[i]);
             }
-            
 
 
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car);
+            }
 
         Console.ReadKey();
         }
