307d39ebd47e3fbc2ca3975cffc24ce35af35a4e
gunayakay <gunayakay@outlook.com>
2023-02-03 21:54:50 +0300

    List of object

--- MyFirstName/Program.cs
+++ MyFirstName/Program.cs
@@ -8,46 +8,35 @@ namespace MyFirstProgram
 
         static void Main(string[] args)
         {
-            // Lists = data structure that represents a list of objects that can be accessed by index
-            //         Similar to arrray, but can dynamically increase/decrease in size 
-            //         using System.Collections.Generic;
 
-            /*
-            String[] food = new String[3];
-
-            food[0] = "pizza";
-            food[1] = "hamburger";
-            food[2] = "hotdog";
-            */
-
-            List<String> food = new List<String>();
-            food.Add("pizza");
-            food.Add("hamburger");
-            food.Add("fries");
-            food.Add("hotdog");
-            food.Add("hamburger");
-
-            String[] foodArray = food.ToArray();
-            //food.Remove("pizza");
-            //food.Insert(0,"sushi");
-            //food.Sort(); 
-            //food.Reverse();
-            //food.Clear();
-            //Console.WriteLine(food.Contains("pizza"));
-            //Console.WriteLine(food.IndexOf("hamburger"));
-            //Console.WriteLine(food.Count);
-            //Console.WriteLine(food.LastIndexOf("hamburger"));
+            List<Player> players = new List<Player>();
 
 
-            foreach (String item in foodArray)
+            players.Add(new Player("Chad"));
+            players.Add(new Player("Steve"));
+            players.Add(new Player("Karen"));
+
+            foreach (Player item in players)
             {
-                Console.WriteLine(item);
+                Console.WriteLine(item.username);
             }
-
-
             Console.ReadKey();
         }
     
+    }
+
+    class Player
+    {
+        public String username;
+
+        public Player(String username)
+        {
+            this.username = username;
+        }
+        public override string ToString()
+        {
+            return username;
+        }
     }
 
 
